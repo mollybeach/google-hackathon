13a53f27d8a70991973424d5b12e51afad3e5e1e
Kathleen Gladstone <kathleengladstone@gmail.com>
2021-03-26 11:56:14 -0400

    Styling for dashboard at desktop

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -10,22 +10,26 @@ const Home = () => {
             <div className='main__component'>
                 <h1 className='main__header'>Welcome back, Olivia</h1>
                 <div className='main__dashboard'>
-                    <div className="main__symptom-checker">
-                        <h4>Have a health concern?</h4>
-                        <p>Check your symptoms fast from the comfort of your home, and get
-                            a recommended course of action.
-                        </p>
-                        <p>If needed, we'll help you find a doctor and book an 
-                            appointment with their clinic.
-                        </p>
-                        <div>
+                    <article className="main__symptom-checker">
+                        <div className="main__symptom-checker-left">
+                            <h4 className="main__subheader">Have a health concern?</h4>
+                            <p className="main__paragraph">
+                                Check your symptoms fast from the comfort of your home, and get
+                                a recommended course of action.
+                            </p>
+                            <p className="main__paragraph">
+                                If needed, we'll help you find a doctor and book an 
+                                appointment with their clinic.
+                            </p>
+                        </div>
+                        <div className="main__symptom-checker-right">
                             <img className="main__graphic" src={Graphic} alt=""/>
                             <button className="main__button">Start checkup</button>
                         </div>
-                    </div>
-                    <div>Div top-right</div>
-                    <div>Div bottom-left</div>
-                    <div>Div bottom-right</div>
+                    </article>
+                    <article className='main__article-two'></article>
+                    <article className='main__article-three'></article>
+                    <article className='main__article-four'></article>
                 </div>
             </div>
         </section>
