312548b3209b9a760cd35bf4497ef72b87edbd67
Farzana Shampa <farzanashampa7@gmail.com>
2021-03-26 12:46:14 -0400

    add PageA

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,50 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
-import './App.css';
+import './App.scss';
 import Home from './components/Home/Home';
 import Profile from './components/Profile/Profile';
 import Header from "./components/Header/Header";
 import Contact from "./components/Contact/Contact";
+import PageA from './components/PageA/PageA';
 import Footer from './components/Footer/Footer';
 
 class App extends Component {
   state = {
-    homeList : null,
+    homeList: null,
     contactList: null,
     profileList: null
-}
+  }
 
-componentDidMount(){
-axios.get('http://localhost:8080/home')
-  .then(res=>{
-  this.setState({
-  homeList :res.data
-  })
-  axios.get('http://localhost:8080/contact')
-  .then(res=>{
-  this.setState({
-  conrtactList :res.data
-})
-})
-}
-)  
-}
+  componentDidMount() {
+    axios.get('http://localhost:8080/home')
+      .then(res => {
+        this.setState({
+          homeList: res.data
+        })
+        axios.get('http://localhost:8080/contact')
+          .then(res => {
+            this.setState({
+              conrtactList: res.data
+            })
+          })
+      }
+      )
+  }
   render() {
-    const {homeList, profileList, contactList} = this.state;
+    const { homeList, profileList, contactList } = this.state;
 
     return (
-      <div className = 'app'>
+      <div className='app'>
         <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path={[`/`, `/home`]} render = {(props)=> <Home homeList = {homeList} contactList={contactList} profileList = {profileList}{...props} />} />
-          <Route exact path={[`/`, `home/profile`]} render = {(props)=> <Profile homeList = {homeList} contactList={contactList} profileList = {profileList}{...props} />} />
-            <Route exact path = '/contact'    render = {(props)=> <Contact homeList = {homeList}  contactList={contactList} {...props} />} />
-        </Switch>
-        <Footer/>
+          <Header />
+          <Switch>
+            <Route exact path={[`/`, `/home`]} render={(props) => <Home homeList={homeList} contactList={contactList} profileList={profileList}{...props} />} />
+            <Route exact path={[`/`, `home/profile`]} render={(props) => <Profile homeList={homeList} contactList={contactList} profileList={profileList}{...props} />} />
+            <Route exact path='/contact' render={(props) => <Contact homeList={homeList} contactList={contactList} {...props} />} />
+            <Route path='/agreement' component={PageA} />
+          </Switch>
+          <Footer />
         </BrowserRouter>
       </div>
     );
