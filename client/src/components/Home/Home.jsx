71f10199d14dfea743878afd5c4d45f713da66e5
Kathleen Gladstone <kathleengladstone@gmail.com>
2021-03-26 09:11:05 -0400

    Color variables, start styling of main page

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,14 +2,33 @@ import React, { Component } from 'react';
 import './Home.scss';
 
 
-class Home extends Component {
+const Home = () => {
     
-    render() {
-        return (
-            <div className = 'home'> Home Page Here
+    return (
+        <section className = 'main'> 
+            <h1 className='main__header'>Welcome back, Olivia</h1>
+            <div className='main__dashboard'>
+                <div className="main__symptom-checker">
+                    <h4>Have a health concern?</h4>
+                    <p>Check your symptoms fast from the comfort of your home, and get
+                        a recommended course of action.
+                    </p>
+                    <p>If needed, we'll help you find a doctor and book an 
+                        appointment with their clinic.
+                    </p>
+                    <div>
+                        <img src="https://via.placeholder.com/200" alt=""/>
+                        <button>Start checkup</button>
+                    </div>
+                    <div>Div top-right</div>
+                    <div>Div bottom-left</div>
+                    <div>Div bottom-right</div>
+                </div>
             </div>
-        );
-    }
+
+        </section>
+    );
 }
+
 
 export default Home;
