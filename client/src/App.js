1f9d54a04e5c631d022ee459c674eb8f4a2785e7
Farzana Shampa <farzanashampa7@gmail.com>
2021-03-26 15:26:45 -0400

    Add PageG

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,44 @@
 import React, { Component } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import axios from 'axios';
 import Home from './components/Home/Home';
 import Profile from './components/Profile/Profile';
-import Header from './components/Header/Header'
+import Header from './components/Header/Header';
+import PageG from './components/PageG/PageG';
 class App extends Component {
   state = {
-    homeList : null,
+    homeList: null,
     contactList: null,
     profileList: null
-}
+  }
 
-componentDidMount(){
-axios.get('http://localhost:8080/home')
-  .then(res=>{
-  this.setState({
-  homeList :res.data
-  })
-  axios.get('http://localhost:8080/contact')
-  .then(res=>{
-  this.setState({
-  conrtactList :res.data
-})
-})
-}
-)  
-}
+  componentDidMount() {
+    axios.get('http://localhost:8080/home')
+      .then(res => {
+        this.setState({
+          homeList: res.data
+        })
+        axios.get('http://localhost:8080/contact')
+          .then(res => {
+            this.setState({
+              conrtactList: res.data
+            })
+          })
+      }
+      )
+  }
   render() {
-    const {homeList, profileList, contactList} = this.state;
+    const { homeList, profileList, contactList } = this.state;
 
     return (
-      <div className = 'app'>
+      <div className='app'>
         <BrowserRouter>
-        <Header/>
-        <Switch>
-          <Route exact path={[`/`, `/home`]} render = {(props)=> <Home homeList = {homeList} contactList={contactList} profileList = {profileList}{...props} />} />
-          <Route exact path={[`/`, `home/profile`]} render = {(props)=> <Profile homeList = {homeList} contactList={contactList} profileList = {profileList}{...props} />} />
-        </Switch>
+          <Header />
+          <Switch>
+            <Route exact path={[`/`, `/home`]} render={(props) => <Home homeList={homeList} contactList={contactList} profileList={profileList}{...props} />} />
+            <Route exact path={[`/`, `home/profile`]} render={(props) => <Profile homeList={homeList} contactList={contactList} profileList={profileList}{...props} />} />
+            <Route path='/pageg' component={PageG} />
+          </Switch>
         </BrowserRouter>
       </div>
     );
