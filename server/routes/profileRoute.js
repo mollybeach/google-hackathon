569c2bf4c59b48082974d386b1722c2eee1fd9b8
mollybeach <56612434+mollybeach@users.noreply.github.com>
2021-03-25 20:30:43 -0700

    BGHack-00: Created profile route stlying and jsx files

---
+++ server/routes/profileRoute.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const dataList = require("../data/bigData.json");
+const router = express.Router();
+const fs = require("fs");
+
+
+
+router.get("/home/profile", (req, res) => {
+        res.status(200).json(dataList);
+  });
+
+
+  router.get("/home/profile/:profileId", (req, res) => {
+    let targethomeId = req.params.profileId;
+    let home = homeList.find(item => {
+    return targethomeId === item.id;
+    });
+    if(home){
+        res.status(200).json(dataList);
+    } else {
+        res.status(400).send({ error: "Item does not exist" });
+      }
+
+})
+
+module.exports = router;
+
