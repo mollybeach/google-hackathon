a1ac2ab0fedd2454a156cf8476624bdea2e90e64
Farzana Shampa <farzanashampa7@gmail.com>
2021-03-26 13:20:55 -0400

    PageC

---
+++ client/src/components/PageC/PageC.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Capture2 from '../../assets/images/Capture2.PNG';
+
+const PageC = () => {
+    return (
+        <section className='symptoms wrapper'>
+            <img src={Capture2} alt='hjk' />
+
+
+        </section>
+    );
+};
+
+export default PageC;
